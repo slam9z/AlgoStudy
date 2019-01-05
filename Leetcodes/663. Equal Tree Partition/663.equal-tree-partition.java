f7a91e6d44bdf805e6f575ec0f6ef44d9d45593b
slam <slam@freewheel.tv>
2019-01-04 21:06:34 -0500

    add LC-662, LC-663, LC-934

--- Leetcodes/663. Equal Tree Partition/663.equal-tree-partition.java
+++ Leetcodes/663. Equal Tree Partition/663.equal-tree-partition.java
@@ -9,6 +9,24 @@
  */
 class Solution {
     public boolean checkEqualTree(TreeNode root) {
+        if (root == null) return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        int total = getSum(root, map);
+        if (total == 0) {
+            return map.getOrDefault(total, 0) > 1;
+        }
         
+        return total % 2 == 0 && map.containsKey(total/2);
+    }
+    
+    private int getSum(TreeNode root, Map<Integer, Integer> map) {
+        if (root == null) return 0;
+        int curSum = root.val;
+        if (root.left != null)
+            curSum += getSum(root.left, map); 
+        if (root.right != null)
+            curSum += getSum(root.right, map);
+        map.put(curSum, map.getOrDefault(curSum, 0) + 1);
+        return curSum;
     }
 }
